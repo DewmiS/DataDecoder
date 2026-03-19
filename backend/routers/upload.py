bbfe58157aa62026d3f1750e23f62672350c1597
DewmiS <dewmisamadhipathiranage@gmail.com>
2026-03-19 20:33:15 +0530

    Refactor upload endpoint

--- backend/routers/upload.py
+++ backend/routers/upload.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
 import io
 import pandas as pd
+from backend.state import session_store
+import uuid
 
 router = APIRouter()
 
 @router.post("/upload")
 async def upload_file(file: UploadFile=File(...)):
+  session_id = str(uuid.uuid4())
   if not file.filename.endswith(".csv"): # type: ignore
     raise HTTPException(status_code=400, detail="Only CSV files are accepted")
   
@@ -20,12 +23,14 @@ async def upload_file(file: UploadFile=File(...)):
       raise HTTPException(status_code=400, detail="Could not parse CSV. Check the file format")
 
   rows, cols = df.shape
+  session_store[session_id] = df
 
   return {
       "filename": file.filename,
       "rows": rows,
       "columns": cols,
-      "column_names": df.columns.tolist()
+      "column_names": df.columns.tolist(),
+      "session_id": session_id
   }
 
 
