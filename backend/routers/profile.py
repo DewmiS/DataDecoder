45b9ce025e7ddc866ef3fd0d2602f358ce1c6e15
DewmiS <dewmisamadhipathiranage@gmail.com>
2026-03-19 21:51:48 +0530

    Refactor profile endpoint

--- backend/routers/profile.py
+++ backend/routers/profile.py
@@ -1,37 +1,49 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, HTTPException
 import pandas as pd
-import io
+from pydantic import BaseModel
+from backend.state import session_store
+
+class sessionRequest(BaseModel):
+  session_id : str
 
 router = APIRouter()
 
 @router.post("/profile")
-async def profile(file: UploadFile=File(...)):
-  if not file.filename.endswith(".csv"): #type:ignore
-    raise HTTPException(status_code=400, detail="Only csv files are accepted.")
-  content =await file.read()
-
-  try:
-    df = pd.read_csv(io.BytesIO(content))
-  except Exception:
-    raise HTTPException(status_code=400, detail="Could not parse CSV. Check the file format")
+async def profile(request: sessionRequest):
+  session_id  = request.session_id    
+  if session_id not in session_store:
+    raise HTTPException(status_code=400, detail="session code not found")
   
+  df = session_store[session_id]
   rows, cols = df.shape
   columns = df.columns.to_list()
   column_details = []
-
+  numeric_count = 0
+  
   for col in columns:
     null_count = int((df[col].isnull().sum()))
+    null_percentage = round(((null_count)/rows)*100,2)
+
     column = {
-      col: str(df[col].dtype),
-      "null_count": null_count,
-      "null_percentage": round(((null_count)/rows)*100,2)
-    }
+        col: str(df[col].dtype),
+        "null_count": null_count,
+        "null_percentage": null_percentage,
+        "high_null_warning": null_percentage>50
+      }
+
+    if pd.api.types.is_numeric_dtype(df[col]):
+      numeric_count=numeric_count+1
+      column["mean"] = round(df[col].mean(), 2)
+      column["median"] = round(df[col].median(),2)
+      column["min"] = round(df[col].min(),2)
+      column["max"] = round(df[col].max(),2)
+
     column_details.append(column)
-    
+
   return{
-    "fileName": file.filename,
     "rows": rows,
     "columns": cols,
     "column_name": df.columns.to_list(),
-    "column_details": column_details
+    "column_details": column_details,
+    "numeric_column_count": numeric_count
   }
