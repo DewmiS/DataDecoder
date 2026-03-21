9798191f5e450791979be497c16ea077233343a0
DewmiS <dewmisamadhipathiranage@gmail.com>
2026-03-21 18:09:02 +0530

    Updated

--- backend/routers/profile.py
+++ backend/routers/profile.py
@@ -16,10 +16,12 @@ async def profile(request: sessionRequest):
   
   df = session_store[session_id]
   rows, cols = df.shape
+  rows = int(rows)
+  cols = int(cols)
   columns = df.columns.to_list()
   column_details = []
   numeric_count = 0
-  
+
   for col in columns:
     null_count = int((df[col].isnull().sum()))
     null_percentage = round(((null_count)/rows)*100,2)
@@ -33,10 +35,10 @@ async def profile(request: sessionRequest):
 
     if pd.api.types.is_numeric_dtype(df[col]):
       numeric_count=numeric_count+1
-      column["mean"] = round(df[col].mean(), 2)
-      column["median"] = round(df[col].median(),2)
-      column["min"] = round(df[col].min(),2)
-      column["max"] = round(df[col].max(),2)
+      column["mean"] = round(float(df[col].mean()), 2)
+      column["median"] = round(float(df[col].median()),2)
+      column["min"] = round(float(df[col].min()),2)
+      column["max"] = round(float(df[col].max()),2)
 
     column_details.append(column)
 
