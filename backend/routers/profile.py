0eebdb8b502b542d04993938563f2baeb38e7f0c
DewmiS <dewmisamadhipathiranage@gmail.com>
2026-03-25 15:47:50 +0530

    Refactor profile endpoint

--- backend/routers/profile.py
+++ backend/routers/profile.py
@@ -1,51 +1,19 @@
 from fastapi import APIRouter, HTTPException
-import pandas as pd
 from pydantic import BaseModel
 from backend.state import session_store
-
-class sessionRequest(BaseModel):
-  session_id : str
+from backend.services.profileService import run_profile
 
 router = APIRouter()
 
+class SessionRequest(BaseModel):
+    session_id: str
+
 @router.post("/profile")
-async def profile(request: sessionRequest):
-  session_id  = request.session_id    
-  if session_id not in session_store:
-    raise HTTPException(status_code=400, detail="session code not found")
-  
-  df = session_store[session_id]
-  rows, cols = df.shape
-  rows = int(rows)
-  cols = int(cols)
-  columns = df.columns.to_list()
-  column_details = []
-  numeric_count = 0
+async def profile(request: SessionRequest):
+    session_id = request.session_id
 
-  for col in columns:
-    null_count = int((df[col].isnull().sum()))
-    null_percentage = round(((null_count)/rows)*100,2)
+    if session_id not in session_store:
+        raise HTTPException(status_code=400, detail="session code not found")
 
-    column = {
-        col: str(df[col].dtype),
-        "null_count": null_count,
-        "null_percentage": null_percentage,
-        "high_null_warning": null_percentage>50
-      }
-
-    if pd.api.types.is_numeric_dtype(df[col]):
-      numeric_count=numeric_count+1
-      column["mean"] = round(float(df[col].mean()), 2)
-      column["median"] = round(float(df[col].median()),2)
-      column["min"] = round(float(df[col].min()),2)
-      column["max"] = round(float(df[col].max()),2)
-
-    column_details.append(column)
-
-  return{
-    "rows": rows,
-    "columns": cols,
-    "column_name": df.columns.to_list(),
-    "column_details": column_details,
-    "numeric_column_count": numeric_count
-  }
+    df = session_store[session_id]
+    return run_profile(df)
