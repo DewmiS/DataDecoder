76c3544da2a0fca0490e4c5ef5ca7d23ae13ff0c
DewmiS <dewmisamadhipathiranage@gmail.com>
2026-04-27 18:17:09 +0530

    Refactor App component to include routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import UploadPage from './pages/UploadPage'
+import { SessionProvider } from './context/SessionContext'
+import ProcessingPage from './pages/ProcessingPage'
+import ProfilePage from './pages/ProfilePage'
+import PatternsPage from './pages/PatternsPage'
+import ClustersPage from './pages/ClustersPage'
+import StoryPage from './pages/StoryPage'
+
 function App() {
   
   return (
     <>
-      
+      <SessionProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<UploadPage />} />
+            <Route path="/processing" element={<ProcessingPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/patterns" element={<PatternsPage />} />
+            <Route path="/clusters" element={<ClustersPage />} />
+            <Route path="/story" element={<StoryPage />} />
+          </Routes>
+        </BrowserRouter>
+      </SessionProvider>
     </>
   )
 }
